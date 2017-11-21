b03b9007e36abb2ae5ec721cf48c1813398fb70f
Paul DESPRES <pdespres@e3r2p6.42.fr>
2017-11-21 12:45:12 +0100

    day 1

--- get_next_line.c
+++ get_next_line.c
@@ -6,40 +6,69 @@
 /*   By: pdespres <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/20 20:58:24 by pdespres          #+#    #+#             */
-/*   Updated: 2017/11/20 21:30:04 by pdespres         ###   ########.fr       */
+/*   Updated: 2017/11/21 10:43:03 by pdespres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-char	*ft_read(int fd)
+int		readf(int fd, char *str)
 {
 	size_t	ret;
-	size_t	buf_size;
 	char	*buf;
-	char	*str;
 	char	*tmp;
 
-	buf_size = BUF_SZ;
-	if (!(str = (char*)malloc(1)))
-		return (NULL);
+	if (str == NULL && !(str = ft_strnew(0)))
+		return (-1);
 	if (!(buf = (char*)malloc(sizeof(*buf) * (BUFF_SIZE + 1))))
-		return (NULL);
-	str[0] = '\0';
-	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+		return (-1);
+	if ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
 		tmp = ft_strjoin(str, buf);
 		free(buf);
 		free(str);
 		str = tmp;
-		if (!(buf = (char*)malloc(sizeof(*buf) * (buf_size + 1))))
-			return (NULL);
 	}
-	return (str);
+	else
+		return (0);
+	return (1);
 }
 
 int		get_next_line(const int fd, char **line)
 {
+	static char	*str = NULL;
+	int			i;
+	char		*ptr;
 
+	i = read(fd, str);
+	if (i <= 0)
+		return (i);
+	ptr = ft_strchr(str, '\n');
+	while (ptr == NULL)
+	{
+		i = read(fd,str);
+		/*	faux pour i = 0	*/
+		if (i <= 0)
+			return (i);
+		ptr = ft_strchr(str, '\n');
+	}
+	*line = ft_strncpy(*line, str, (ptr - str - 1));
+	ptr = ft_strsub(str, (ptr - str + 1), ft_strlen(ptr) - 1);
+	free (str);
+	str = ptr;
+	free (ptr);
+	return(1);
+}
+
+int		main(int ac, char **av)
+{
+	char	*str;
+	int		fd;
+
+	fd = open(av[1], O_RDONLY);
+	get_next_line(fd, &str);
+	printf("%s\n", str);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,14 @@
 /*   By: pdespres <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/20 21:00:28 by pdespres          #+#    #+#             */
-/*   Updated: 2017/11/20 21:17:53 by pdespres         ###   ########.fr       */
+/*   Updated: 2017/11/21 10:42:59 by pdespres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <stdlib.h>
 
 # define BUFF_SIZE 32
 
